09581a94d01e34e8756d8fc8190d0bbe5ef49af1
diorbek <diorbeknorhuja@gmail.com>
2025-01-14 14:27:21 +0500

    otlar proyektini bishladm

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Button from 'react-bootstrap/Button';
+
+// or less ideally
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='container'>
+        salom
+      </div>
+    </>
+  )
+}
+
+export default App
